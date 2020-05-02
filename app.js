7bd6b496c3169ea9d834f1b51ab088d15f2cf14c
Unnati Kolhe <unnatikolhe@gmail.com>
2020-05-02 16:09:15 +0530

    Add filess

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = [];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+  const day = date.getDate();
+
+  res.render("list", { listTitle: day, newListItems: items });
+
+});
+
+app.get("/work",function(req,res){
+  res.render("list", { listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work",function(req,res){
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.post("/",function(req,res){
+  const item = req.body.newItem;
+
+  if(req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.listen(3000, function(req, res) {
+  console.log("Sever Started!");
+});
